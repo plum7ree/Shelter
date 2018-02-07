4415187b7e33ec5eb755f8c93121ef5927cd5f90
plum7ree <lomojiki@gmail.com>
2018-02-06 23:01:13 -0500

    made a new repo for m4

---
+++ app/src/main/java/cs2340/gatech/edu/lab4/controller/MainActivity.java
@@ -0,0 +1,37 @@
+package cs2340.gatech.edu.lab4.controller;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import cs2340.gatech.edu.lab4.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        Button registerButton = (Button) findViewById(R.id.register_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
