32aa8bee0fe14d4ef852acbab683fa9d0b233c78
plum7ree <lomojiki@gmail.com>
2018-03-14 22:41:03 -0400

    Combine User class and Admin class to Account class. So don't need to have seperate ArrayList users and admins

--- app/src/main/java/cs2340/gatech/edu/lab4/model/Account.java
+++ app/src/main/java/cs2340/gatech/edu/lab4/model/Account.java
@@ -7,22 +7,34 @@ import java.util.List;
  * Created by Zumong on 2/16/18.
  */
 
-public abstract class Account {
-    public Account() {
+public class Account {
 
+    private String _username;
+    private String _password;
+    AccountType _type;
+
+    public Account(String username, String pass, AccountType t) {
+        _username = username;
+        _password = pass;
+        _type = t;
     }
     public static List<AccountType> legalAccountTypes = Arrays.asList(AccountType.USER,AccountType.ADMIN);
 
-    String username;
-    String password;
-    AccountType type;
+    public String getUsername() { return _username; }
+    public String getPassword() { return _password; }
+
+
+    public AccountType getAccountType() {
+        return _type;
+    }
+    public String toString() {
+        return _username + " " + _password + " " + _type;
+    }
 
     @Override
     public boolean equals(Object o) {
         Account account = (Account) o;
-        return account.username.equals(((Account) o).username);
+        return account._username.equals(((Account) o).getUsername());
     }
 
-    public String getUsername() { return username; }
-    public String getPassword() { return password; }
 }
